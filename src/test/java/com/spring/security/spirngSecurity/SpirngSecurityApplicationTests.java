14027b8900be5ac1907d34a55bb56254f9e6795c
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-28 15:14:57 +0530

    first commit

---
+++ src/test/java/com/spring/security/spirngSecurity/SpirngSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.spirngSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpirngSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
